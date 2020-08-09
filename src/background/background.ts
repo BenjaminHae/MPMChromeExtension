4139b568da104da51e06608506ddb8305c64e8a5
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-08-09 16:57:04 +0200

    add description of background services

---
+++ src/background/background.ts
@@ -0,0 +1,26 @@
+
+//todo
+// context
+//   contextEntries
+// activeTab
+//   insertTextIntoInput
+// backgroundListener
+//   listen to popup
+//     hostUrl 
+//     IsLoggedIn
+//     GetAvailableAccounts (send URL retrieve accounts)
+//     SelectAccount
+//     SetAction (Edit/Add for normal webpage)
+//     CopyPasswordToClipboard
+//   listen to content
+//     only when this is the url of password manager instance
+//     receiveCrypto
+//     Logout
+//     FetchActions
+//     setHost
+//     Options
+//     selectAccount
+
+// hostCommunication
+//   fetchAccounts using backend classes
+//   getData from accounts
