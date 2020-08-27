20ba3a880375e6f77cf9e4c2ef13a1000120de2b
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-08-27 19:38:43 +0200

    add options page

--- src/options/index.ts
+++ src/options/index.ts
@@ -1,1 +1,38 @@
-console.log('options hello');
+function storage() {
+  if ("sync" in chrome.storage)
+    return chrome.storage.sync;
+  return chrome.storage.local;
+}
+function loadData() {
+  storage().get({url: ""}, function(items) {
+    (document.getElementById('url') as HTMLInputElement).value = items.url;
+    console.log(items);
+  });
+}
+function storeData() {
+  let url = (document.getElementById('url') as HTMLInputElement).value;
+  storage().set({url: url}, function() {
+    document.getElementById('status').textContent = 'Options saved.';
+  });
+}
+function createHTML() {
+  let label = document.createElement('label');
+  label.setAttribute('for','url'); 
+  label.textContent = 'Password-Manager URL:'; 
+  document.body.appendChild(label);
+  let input = document.createElement('input');
+  input.setAttribute('type','url'); 
+  input.setAttribute('id','url'); 
+  input.setAttribute('placeholder','https://www.yourdomain.com/'); 
+  document.body.appendChild(input);
+  let button = document.createElement('button');
+  button.setAttribute('id','save'); 
+  button.textContent = 'store'; 
+  button.onclick = storeData;
+  document.body.appendChild(button);
+  let status = document.createElement('p');
+  status.setAttribute('id','status'); 
+  document.body.appendChild(status);
+}
+createHTML();
+loadData();
