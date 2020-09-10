3d8a0831d68d407a376bea5eaf3bf696bd73097b
Benjamin Häublein <benjaminhaeublein@gmail.com>
2020-09-10 19:55:48 +0200

    add getUserSession to IFrame

--- src/background/IFrameConnector.ts
+++ src/background/IFrameConnector.ts
@@ -2,6 +2,7 @@ import SparseAccount from '../models/SparseAccount';
 
 interface methods {
   setUserSession: (username: string, key: any) => void; 
+  getUserSession: () => {username: string, key: any}; 
 }
 class IFrameConnector {
   private meth: methods;
@@ -35,13 +36,17 @@ class IFrameConnector {
         return;
       }
       console.log("received message from iframe" );
-      this.handleFrameRequest(event.data["request"], event.data["data"]);
+      this.handleFrameRequest(event.data["request"], event.data["data"], event.source);
     });
   }
-  handleFrameRequest(request: string, data: object) {
+  handleFrameRequest(request: string, data: object, source: any) {
     switch(request){
       case "session": 
         this.meth.setUserSession(data["username"], data["key"]); 
+        break;
+      case "retreiveSession":
+        let session = this.meth.getUserSession();
+        source.postMessage({request: "session", to: "page", data: session}, "*");
         break;
     }
   }
