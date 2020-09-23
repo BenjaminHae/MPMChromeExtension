dfc5cae23335f81a25d30d90f3d17d214ce49fdb
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-09-23 10:35:15 +0200

    change to Extension in backend

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -4,17 +4,15 @@ import { ICredentialProvider } from '../backend/controller/credentialProvider';
 import { Account } from '../backend/models/account';
 import Action from '../models/Action';
 
-interface IPluginSystem {
-  backendLogin(credentialProvider: ICredentialProvider, username?: string): void;
-  logout(): void;
-  registerPlugin(plugin: any): void;
-  getAccountByIndex(index:number): Account;
-  UIeditAccountSelect(account: Account): void;
-  UIaddAccountSelect(proposals: {[index: string]:string}): void;
+interface IBrowserExtensionPlugin {
+  doLogin: (username: string, key: CryptoKey) => void;
+  setAction: (action: Action) => void;
+  setActive: () => void;
 }
 
-interface WindowWithPluginSystem { pluginSystem: IPluginSystem; }
+interface WindowWithPluginSystem { browserExtensionPlugin?: IBrowserExtensionPlugin; }
 
+// todo: create on first request
 let extensionConnector = new ExtensionConnector();
 
 // listen to:
@@ -23,9 +21,6 @@ let extensionConnector = new ExtensionConnector();
 // Actions:
 //   wrongHost?? Set Wrong host in content-script ?
 
-// registered Events in MPM pluginSystem
-// drawAccount -> add select in extension button
-
 function sendEvent(request: string, data?: object) {
   let evt = new CustomEvent('MPMExtensionEventToPlugin', {detail:{request: request, data: data}});
   document.dispatchEvent(evt);
@@ -33,25 +28,36 @@ function sendEvent(request: string, data?: object) {
 
 //todo make one generic event with a random prefix. Handle specific type of event in data
 document.addEventListener('MPMExtensionEventToContentScript', async (e: CustomEvent) => {
-    console.log(e);
     switch (e.detail.request) {
-      case "loginSuccessful": 
-        let iframeConnector = new IFrameConnector();
-        iframeConnector.sendSession(e.detail.data);
+      case "hostMatches":
+        if (await extensionConnector.getHostMatches()) {
+          sendEvent("hostMatches");
+        }
         break;
       case "loginViewReady":
         if (await extensionConnector.requestLogin()) {
-          let iframeConnector = new IFrameConnector({setSession: (username, key)=>{sendEvent("session", {username: username, key: key})}});
+          let iframeConnector = new IFrameConnector({
+            setSession: (username, key) => {
+                sendEvent("session", {username: username, key: key});
+                iframeConnector.removeIFrame();
+              }
+            });
           iframeConnector.retreiveSession();//todo add random identifier that is then sent back
         }
+        break;
+      case "loginSuccessful": 
+        let iframeConnector = new IFrameConnector();
+        iframeConnector.sendSession(e.detail.data);
+        window.setTimeout(() => iframeConnector.removeIFrame(), 1000);
         break;
       case "accountViewReady":
         let action: Action;
         if (action = await extensionConnector.getAction()) {
-          console.log("sending action to plugin");
-          console.log(action);
           sendEvent("action", action);
         }
+        break;
+      case "selectAccount":
+        extensionConnector.selectAccount(e.detail.data.index);
         break;
       case "logout":
         extensionConnector.logout();
@@ -70,88 +76,14 @@ function executeScript(script, args = null) {
 }
 
 executeScript(function() {
-  const pluginSystem = ((window as unknown) as WindowWithPluginSystem).pluginSystem
+  const plugin = ((window as unknown) as WindowWithPluginSystem).browserExtensionPlugin
   // todo maybe check something else to find out whether this is the correct password manager instance (but this should also be checked in background)
-  if (pluginSystem) {
-    class BrowserExtensionPlugin {
-      private actionsReceived: boolean = false;
-      private accountsLoaded: boolean = false;
-      private action: Action;
-
-      constructor(private pluginSystem: IPluginSystem) {
-      }
-
-      private sendEvent(request: string, data?: object) {
-        let evt = new CustomEvent('MPMExtensionEventToContentScript', {detail:{request: request, data: data}});
-        document.dispatchEvent(evt);
-      }
-      loginSuccessful(username: string, key: any): void {
-        console.log("login Successful");
-        this.sendEvent('loginSuccessful', {username: username, key: key});
-      }
-
-      loginViewReady() {
-        console.log("login view ready");
-        this.sendEvent('loginViewReady');
-      }
-
-      // this callback reacts to accounts in backend being ready
-      // it is possibly necessary to react to the "account view" being ready
-      accountsReady() {
-        this.accountsLoaded = true;
-        if (this.actionsReceived) {
-          this.performAction();
-        }
-        this.sendEvent("accountViewReady");
-      }
-
-      preLogout() {
-        this.sendEvent("logout");
-      }
-
-      performAction() {
-        switch (this.action.action) {
-          case "logout": 
-            this.pluginSystem.logout();
-            break;
-          case "edit": 
-            let account: Account;
-            if (account = this.pluginSystem.getAccountByIndex(this.action.data.index)) {
-              console.log(`found account by id ${account.index}`);
-              this.pluginSystem.UIeditAccountSelect(account);
-            }
-            else {
-              console.log("did not find account by id");
-              console.log(this.action);
-            }
-            break;
-          case "add":
-            this.pluginSystem.UIaddAccountSelect(this.action.data);
-            break;
-        }
-      }
-
-      setAction(action: Action) {
-        this.action = action;
-        this.actionsReceived = true;
-        if (this.accountsLoaded) {
-          this.performAction();
-        }
-      }
-
-      doLogin(username: string, key: CryptoKey) {
-        let credentials = {
-          getKey: () => key,
-          cleanUp: () => Promise.resolve()
-        };
-        this.pluginSystem.backendLogin(credentials);
-      }
-    }
-    const plugin = new BrowserExtensionPlugin(pluginSystem);
-    pluginSystem.registerPlugin(plugin);
+  if (plugin) {
     document.addEventListener('MPMExtensionEventToPlugin', (e: CustomEvent) => {
-        console.log(e);
         switch (e.detail.request) {
+          case "hostMatches": 
+            plugin.setActive();
+            break;
           case "session": 
             plugin.doLogin(e.detail.data.username, e.detail.data.key);
             break;
@@ -161,6 +93,8 @@ executeScript(function() {
         }
       }, 
       false);
+    let evt = new CustomEvent('MPMExtensionEventToContentScript', {detail:{request: "hostMatches"}});
+    document.dispatchEvent(evt);
   }
 });
 
